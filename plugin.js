76f073150bb7051b4fc1eb8756762eacc7a5c58f
Ripon <habibkazi92@gmail.com>
2021-01-28 06:42:04 +0800

    all ok except remove feature

--- plugin.js
+++ plugin.js
@@ -137,3 +137,22 @@ function getInputValue(product){
     const productInputCount = parseInt(productInput.value);
     return productInputCount;
 }
+
+// function getTotal(phone, casing){
+//     const phonePrice = 500;
+//     const casingPrice = 50;
+//     const phoneCost = phone * phonePrice;
+//     const casingCost = casing * casingPrice; 
+//     const total = phoneCost + casingCost;
+//     const totalElement = document.getElementById("totalCost");
+//     // console.log(phoneCost, casingCost);
+//     totalElement.innerText = total;
+//     return total;
+
+// }
+
+// const totalCost = getTotal(5, 3);
+// const firstResult = document.getElementById("firstResult");
+// firstResult.innerText = totalCost;
+// const secondResult = document.getElementById("firstResult");
+// secondResult.innerText = totalCost;
